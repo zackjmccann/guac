3d47613f2ac5903a6f769f71b2cd606b78f92ef8
zackjmccann <zackjmccann@gmail.com>
2022-10-15 22:38:21 +0000

    tests updated

--- tests/test_get_service_account/test_get_service_account.py
+++ tests/test_get_service_account/test_get_service_account.py
@@ -2,8 +2,10 @@ from guac.google_connections import get_service_account
 
 
 def test_get_service_account_path():
-    assert get_service_account('path') == '/Users/zackmccann/JamesMatter/guac-dev/client_secrets.json'
+    """Test retrievial of service account credential from path"""
+    assert get_service_account('path') == './client_secrets.json'
 
 
 def test_get_service_account_json():
-    assert type(get_service_account('json')) == dict
+    """Test retrievial of service account credentials from json"""
+    assert isinstance(get_service_account('json'), dict)
--- tests/test_reporting_api/test_reporting_api_connection.py
+++ tests/test_reporting_api/test_reporting_api_connection.py
@@ -2,4 +2,5 @@ from guac.google_connections import connect_reporting_api
 
 
 def test_connect_reporting_api():
+    """Test connection abilities to the Google Analyytics Reporting API"""
     assert len(connect_reporting_api()['reports']) > 0
